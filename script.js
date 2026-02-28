02477ffc814fcb7a324761c61a8f6c0c17dd879f
Dieter Foos <d.foos@mail.de>
2026-02-28 20:59:08 +0100

    about section created

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ document.addEventListener("mousemove", (e) => {
 });
 
 
+// Language switch 
+const langBtn = document.querySelector('.lang-btn');
+let isDE = false;
+
+const imgs = {
+  en_default: '/assets/img/icons/lang_switch_default.svg',
+  en_hover: '/assets/img/icons/lang_switch_default_hover.svg',
+  de_default: '/assets/img/icons/lang_switch_german.svg',
+  de_hover: '/assets/img/icons/lang_switch_german_hover.svg',
+}
+
+langBtn.addEventListener('mouseenter', () => {
+  langBtn.src = isDE ? imgs.de_hover : imgs.en_hover;
+});
+
+langBtn.addEventListener('mouseleave', () => {
+  langBtn.src = isDE ? imgs.de_default : imgs.en_default;
+});
+
+langBtn.addEventListener('click', () => {
+  isDE = !isDE;
+  langBtn.src = isDE ? imgs.de_hover : imgs.en_hover;
+});
+
+
+
 
 // Dark / Ligth mode
 /* const toggle = document.querySelector('#theme-toggle');
