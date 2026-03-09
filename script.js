0324234dcc1882e76ea4df37a4d0437acb6589e7
Dieter Foos <d.foos@mail.de>
2026-03-09 20:11:38 +0100

    lang switch updated

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Language switch 
+const button = document.getElementById('toggle-btn');
+let isEn = localStorage.getItem('toggleState') === 'true';
+
+updateButton(isEn);
+
+button.addEventListener('click', () => {
+  isEn = !isEn;
+  localStorage.setItem('toggleState', isEn);
+  updateButton(isEn);
+});
+
+function updateButton(state) {
+  button.classList.toggle('active', state);
+}
+
+
+
+
+
+
+
+
 // Mouse gradient effect
 document.addEventListener("mousemove", (e) => {
   document.documentElement.style.setProperty("--cursor-x", e.clientX + "px");
@@ -10,38 +33,6 @@ document.addEventListener('mouseleave', () => {
 
 document.addEventListener('mouseenter', () => {
   document.documentElement.style.setProperty('--cursor-opacity', '1');
-});
-
-
-// Language switch 
-const wrapper = document.querySelector('.lang-wrapper');
-let isDE = false;
-let isClicking = false;
-
-const show = (selector) => {
-  document.querySelectorAll('.lang-img').forEach(img => img.style.opacity = '0');
-  document.querySelector(selector).style.opacity = '1';
-};
-
-show('.lang-en-default');
-
-wrapper.addEventListener('mouseenter', () => {
-  show(isDE ? '.lang-de-hover' : '.lang-en-hover');
-});
-
-wrapper.addEventListener('mouseleave', () => {
-  if (isClicking) return;
-  show(isDE ? '.lang-de-default' : '.lang-en-default');
-});
-
-wrapper.addEventListener('mousedown', () => {
-  isClicking = true;
-});
-
-wrapper.addEventListener('click', () => {
-  isDE = !isDE;
-  show(isDE ? '.lang-de-hover' : '.lang-en-hover');
-  setTimeout(() => { isClicking = false; }, 50);
 });
 
 
